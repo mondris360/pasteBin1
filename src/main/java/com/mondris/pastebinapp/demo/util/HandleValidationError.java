f50bfcaa03638e04627ea8d80ca0a75454477da4
Mondris <justmondris@gmail.com>
2021-01-15 03:15:12 +0100

    Remove redis cache dependency and config - no need of redis since the object changes too often

--- src/main/java/com/mondris/pastebinapp/demo/util/HandleValidationError.java
+++ src/main/java/com/mondris/pastebinapp/demo/util/HandleValidationError.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -15,7 +14,6 @@ import java.util.Map;
 public class HandleValidationError extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
